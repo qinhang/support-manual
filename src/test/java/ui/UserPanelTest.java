a5d97950708ef75eeb77e368113f31c4301781b5
Freja <ffreja88@gmail.com>
2014-10-28 21:10:39 -0500

    edited gradle settings to make test work

--- src/test/java/ui/UserPanelTest.java
+++ src/test/java/ui/UserPanelTest.java
@@ -1,5 +1,15 @@
 package ui;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class UserPanelTest{
 
+    @Test
+    public void shouldImportExcel() throws Exception {
+
+
+    }
 }
